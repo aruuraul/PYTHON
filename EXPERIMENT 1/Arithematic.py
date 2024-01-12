4b30e95e80b711ececa2c970884aea541f0b84c5
aruuraul <139954540+aruuraul@users.noreply.github.com>
2024-01-12 13:04:06 +0530

    Add files via upload

---
+++ EXPERIMENT 1/Arithematic.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter the first no"))
+num2=int(input("Enter the second no"))
+a = num1+num2
+print ('sum is',a)
+b = num1-num2
+print('difference is',b)
+c = num1*num2
+print ('product is',c)
+d = (num1/num2)
+print ('Division is',d)
+e = num1%num2
+print ('modulos is',e)
+f = num1**num2
+print ('Exponential is',f)
+g = num1//num2
+print ('Floor Divivsion is',g)
